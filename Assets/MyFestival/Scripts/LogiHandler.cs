7a713682f8e443fcf41f6787eca9db16562cad2a
chanoo <fastchanoo@gmail.com>
2022-11-09 21:03:32 +0900

    http 모듈 추가

--- Assets/MyFestival/Scripts/LogiHandler.cs
+++ Assets/MyFestival/Scripts/LogiHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Duck.Http;
 
 public class LogiHandler : MonoBehaviour
 {
@@ -23,6 +24,16 @@ public class LogiHandler : MonoBehaviour
     {
         Debug.Log(text + " - 글자 입력 중");
         email_inputField.text = text;
+    }
+
+    public void OnClickLogin()
+    {
+        var request = Http.Get("http://mywebapi.com/")
+            .SetHeader("Authorization", "username:password")
+            .OnSuccess(response => Debug.Log(response.Text))
+            .OnError(response => Debug.Log(response.StatusCode))
+            .OnDownloadProgress(progress => Debug.Log(progress))
+            .Send();
     }
 
 }
